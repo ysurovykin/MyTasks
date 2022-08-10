99c950c0d9907bd16692b330ccda7a5f36fcf292
Yurii Surovykin <ysurovykin@gmail.com>
2022-08-10 16:57:54 +0300

    Add responsive task playlists page

---
+++ client/src/components/playlist/index.js
@@ -0,0 +1,10 @@
+function Playlist() {
+    return(
+        <div className="playlist-wrapper">
+            <h3>Title</h3>
+            <div className="playlist"></div>
+        </div>
+    )
+}
+
+export default Playlist;
--- client/src/components/taskPlaylist/index.js
+++ client/src/components/taskPlaylist/index.js
@@ -1,10 +1,25 @@
+import Playlist from '../playlist'
 import './taskPlaylist.scss'
 
 function taskPlaylist() {
-    return(
-    <>
-    Task Playlist
-    </>
+    return (
+        <div className='task-playlists-wrapper'>
+            <h1>Task playlists</h1>
+            <div className='task-playlists-wrapper__playlists'>
+                <Playlist />
+                <Playlist />
+                <Playlist />
+                <Playlist />
+                <Playlist />
+                <Playlist />
+                <Playlist />
+                <div className="task-playlists-wrapper__add-playlist-wrapper">
+                    <div className="playlist">
+                        <img src='./images/plus-circle128x128.png'/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
